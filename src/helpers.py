b8750827d5ec99585cfdccbf70486f1e673c3701
Stephen Kisee <102782199+StephKisee@users.noreply.github.com>
2024-01-07 18:35:49 +0300

    Added helper functions.

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,34 @@
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+import scipy.stats as stats
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+fonts = fm.findSystemFonts()
+
+for font in fonts:
+    fm.fontManager.addfont(font)
+
+font_size = 11
+font_family = 'Times New Roman'
+plot_style = 'seaborn-v0_8-paper'
+# plot_style = 'fivethirtyeight'
+
+plt.style.use(plot_style)
+plt.rcParams['grid.alpha'] = 0.25
+plt.rcParams['font.family'] = font_family
+# plt.rcParams['font.size'] = font_size
+# plt.rcParams['axes.labelsize'] = font_size
+# plt.rcParams['xtick.labelsize'] = font_size
+# plt.rcParams['ytick.labelsize'] = font_size
+# plt.rcParams['legend.fontsize'] = font_size
+# plt.rcParams['figure.titlesize'] = font_size
+# plt.rcParams['axes.titlesize'] = font_size + 1
+plt.rcParams['axes.spines.top'] = True
+plt.rcParams['axes.spines.right'] = True
+plt.rcParams['figure.constrained_layout.use'] = True
 
 
 def engineer_change_features(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -40,60 +70,135 @@ def engineer_change_features(dataframe: pd.DataFrame) -> pd.DataFrame:
                 dataframe.insert(change_col_index, change_col, change_col_data)
     return dataframe
 
-
-def create_composite_feature(data, variables, method='mean', weights=None):
+def create_composite_feature(dataframe, features, new_feature_name,
+                             method='mean', weights=None, drop_features=False):
     """
-    Create a composite feature from a set of variables.
+    Create a composite feature from a list of features.
 
     Parameters
     ----------
-    data : pandas.DataFrame
-        The dataframe containing the variables.
-    variables : list
-        The list of variables to combine.
-    method : str, optional
-        The method to use to combine the variables ('mean', 'sum', 'weighted_mean', 'weighted_sum', 'interaction', 'pca').
-    weights : list, optional
-        The list of weights to use for the variables. The default is None (only used for 'weighted_mean' and 'weighted_sum').
+    dataframe : pandas.DataFrame
+        The dataframe containing the features.
+    features : list
+        The list of features to combine.
+    new_feature_name : str
+        The name of the new feature.
+    drop_features : bool, default False
+        Whether to drop the features used to create the new feature.
 
     Returns
     -------
-    data : pandas.DataFrame
-        The dataframe with the new composite feature.
-
-    Examples
-    --------
-    >>> data = create_composite_feature(data, ['var1', 'var2', 'var3'], method='weighted_mean', weights=[0.5, 0.3, 0.2])
-    >>> data = create_composite_feature(data, ['var1', 'var2', 'var3'], method='pca')
-    >>> data = create_composite_feature(data, ['var1', 'var2', 'var3'], method='interaction')
-    >>> data = create_composite_feature(data, ['var1', 'var2', 'var3'], method='sum')
+    dataframe : pandas.DataFrame
+        The dataframe with the new feature added.
     """
+    dataframe = dataframe.copy()
 
-    # Import library to create a composite feature
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.decomposition import PCA
+    if new_feature_name in dataframe.columns:
+        raise ValueError(f'Feature {new_feature_name} already exists.')
 
-    # Create a composite feature
+    if not all(feature in dataframe.columns for feature in features):
+        raise ValueError('Not all features in features exist in dataframe.')
+
     if method == 'mean':
-        data['composite'] = data[variables].mean(axis=1)
-    elif method == 'sum':
-        data[f'composite ({method})'] = data[variables].sum(axis=1)
-    elif method == 'weighted_mean':
-        data[f'composite ({method})'] = np.average(data[variables], axis=1, weights=weights)
-    elif method == 'weighted_sum':
-        data[f'composite ({method})'] = np.dot(data[variables], weights)
-    elif method == 'interaction':
-        data[f'composite ({method})'] = data[variables].prod(axis=1)
-    elif method == 'pca':
-        # Standardize the data
-        data[variables] = StandardScaler().fit_transform(data[variables])
+        dataframe[new_feature_name] = dataframe[features].mean(axis=1)
 
-        # Create a PCA object
+    elif method == 'sum':
+        dataframe[new_feature_name] = dataframe[features].sum(axis=1)
+
+    elif method == 'weighted_mean':
+        if weights is None:
+            raise ValueError('Weights must be provided for weighted_mean method.')
+        dataframe[new_feature_name] = np.average(dataframe[features], axis=1, weights=weights)
+
+    elif method == 'weighted_sum':
+        if weights is None:
+            raise ValueError('Weights must be provided for weighted_sum method.')
+        dataframe[new_feature_name] = np.dot(dataframe[features], weights)
+
+    elif method == 'interaction':
+        dataframe[new_feature_name] = dataframe[features].prod(axis=1)
+
+    elif method == 'pca':
+        dataframe[features] = StandardScaler().fit_transform(dataframe[features])
+
         pca = PCA(n_components=1)
 
-        # Fit the PCA object
-        data[f'composite ({method})'] = pca.fit_transform(data[variables])
-    else:
-        raise ValueError('Invalid method.')
+        dataframe[new_feature_name] = pca.fit_transform(dataframe[features])
 
-    return data
+    if drop_features:
+        dataframe.drop(columns=features, inplace=True)
+
+    return dataframe
+
+def transform_features(dataframe, features,
+                       treat_outliers='drop', treat_skewness=None):
+    """
+    Transform features in a dataframe.
+
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        The dataframe containing the features.
+
+    features : list
+        The list of features to transform.
+
+    treat_outliers : any, default None
+        How to treat outliers. If None, no treatment is applied.
+
+    treat_skewness : any, default None
+        How to treat skewness. If None, no treatment is applied.
+
+    Returns
+    -------
+    dataframe : pandas.DataFrame
+        The dataframe with the transformed features.
+    """
+    dataframe = dataframe.copy()
+
+    if not all(feature in dataframe.columns for feature in features):
+        raise ValueError('Not all features in features exist in dataframe.')
+
+    if features is None:
+        features = dataframe.columns.tolist()
+
+    if not isinstance(features, list):
+        features = list(features)
+
+    for feature in features:
+
+        if treat_outliers is not None:
+            outliers = dataframe[(np.abs(stats.zscore(dataframe[feature])) > 3)]
+
+            if treat_outliers == 'drop':
+                dataframe.drop(outliers.index, inplace=True)
+
+            elif treat_outliers == 'mean':
+                dataframe[feature] = np.where(np.abs(stats.zscore(dataframe[feature])) > 3,
+                                              dataframe[feature].mean(),
+                                              dataframe[feature])
+
+            elif treat_outliers == 'median':
+                dataframe[feature] = np.where(np.abs(stats.zscore(dataframe[feature])) > 3,
+                                              dataframe[feature].median(),
+                                              dataframe[feature])
+
+            elif treat_outliers == 'winsorize':
+                dataframe[feature] = stats.mstats.winsorize(dataframe[feature], limits=0.03)
+
+        if treat_skewness is not None:
+            const = np.abs(dataframe[feature].min()) + 1
+
+            if treat_skewness == 'cuberoot':
+                dataframe[f'{feature} (cbrt)'] = np.cbrt(dataframe[feature])
+
+            elif treat_skewness == 'sqrt':
+                dataframe[f'{feature} (sqrt)'] = np.sqrt(dataframe[feature] + const)
+
+            elif treat_skewness == 'log':
+                dataframe[f'{feature} (log)'] = np.log(dataframe[feature] + const)
+
+            elif treat_skewness == 'boxcox':
+                dataframe[f'{feature} (boxcox)'] = stats.boxcox(dataframe[feature] + const)[0]
+
+    return dataframe
